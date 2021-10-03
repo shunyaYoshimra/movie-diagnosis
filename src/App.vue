edf9055f08953362c19e82d1dd96cd5fad5671c2
shunyaYoshimura <maoorgri1015@gmail.com>
2021-10-03 15:28:43 +0900

    import Main.vue to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <Main></Main>
+  </div>
 </template>
 
 <script>
+import Main from "./components/Main.vue";
 export default {
+  components: {
+    Main,
+  },
   data() {
     return {};
   },
